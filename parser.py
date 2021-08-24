201694396435640a58e0ed50262312b4dbd5c608
Valhalleon <v.galuta@gmail.com>
2021-08-25 00:18:26 +0300

    Parsing finished

--- parser.py
+++ parser.py
@@ -11,30 +11,56 @@ HEADERS = {
 def get_html(url, params=None):
   return requests.get(url, headers=HEADERS, params=params)
 
+def get_pages_count(html):
+  soup = BeautifulSoup(html, 'html.parser')
+  pagination = soup.find_all('span', class_='mhide')
+
+  if pagination:
+    return int(pagination[-1].get_text())
+  else:
+    return 1
+
 def get_content(html):
   soup = BeautifulSoup(html, 'html.parser')
   items = soup.find_all('section', class_='proposition')
 
-  cars = []
+  content = []
   for item in items:
     title = item.find('h3', class_='proposition_name')
     link = item.find('a', class_='proposition_link')
     price = item.find('div', class_='proposition_price')
     price_usd = price.find('span', class_='size22') if price else False
     price_uah = price.find('span', class_='size16') if price else False
+    region = item.find('span', class_='region') if price else False
 
-    cars.append({
+    content.append({
       'title': title.get_text(strip=True),
       'link': HOST + link.get('href') if link else '',
       'price usd': price_usd.get_text(strip=True) if price_usd else '',
       'price uah': price_uah.get_text(strip=True) if price_uah else '',
+      'region': region.get_text(strip=True) if region else '',
     })
-  print(cars)
+
+  return content
 
 def parse():
   html = get_html(URL)
   if html.status_code == 200:
-    get_content(html.text)
+    rows = []
+    pages_count = get_pages_count(html.text)
+
+    for page in range(1, pages_count + 1):
+      print(f'Parsing page {page} from {pages_count}...')
+
+      params = {
+        page: page
+      }
+      html = get_html(URL, params=params)
+      content = get_content(html.text)
+      rows.extend(content)
+
+    print(f'Got {len(rows)} data rows')
+
   else:
     print('Error:', html.status_code)
 
