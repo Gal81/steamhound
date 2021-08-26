b08017933b2fa4833ed9b9de56c94b9a53aedaee
Valhalleon <v.galuta@gmail.com>
2021-08-26 23:41:18 +0300

    Add json to html method

--- parser.py
+++ parser.py
@@ -1,26 +1,16 @@
-import csv
 import requests
 from bs4 import BeautifulSoup
+from json2html import *
 
-# HOST = 'https://bitskins.com'
 URL = 'https://bitskins.com'
 HEADERS = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
   'accept': '*/*'
 }
-FILE = 'data.csv'
+FILE = 'dump/data.html'
 
 def get_html(url, params=None):
   return requests.get(url, headers=HEADERS, params=params)
-
-def get_pages_count(html):
-  soup = BeautifulSoup(html, 'html.parser')
-  pagination = soup.find_all('span', class_='mhide')
-
-  if pagination:
-    return int(pagination[-1].get_text())
-  else:
-    return 1
 
 def get_content(html):
   soup = BeautifulSoup(html, 'html.parser')
@@ -31,54 +21,50 @@ def get_content(html):
     title = item.find('div', class_='item-title')
     price = item.find('span', class_='item-price-display')
     wear = item.find('span', class_='unwrappable-float-pointer')
-    flot = wear.get_text(strip=True).split(',')[0] if wear else 'Nope'
+    floAt = wear.get_text(strip=True).split(',')[0] if wear else 'Nope'
     # link = item.find('span', class_='unwrappable-float-pointer')
 
-    contentItem = {
-      'title': title.get_text(strip=True),
-      'price': price.get_text(strip=True),
-      'flot': flot,
-    }
+    if floAt.startswith('0.00'):
+      contentItem = {
+        'title': title.get_text(strip=True),
+        'price': price.get_text(strip=True),
+        'float': floAt,
+      }
 
-    print(contentItem)
+      print(contentItem)
 
-    content.append(contentItem)
+      content.append(contentItem)
 
   return content
 
 def save_file(items, path):
-  with open(path, 'w', newline='') as file:
-    writer = csv.writer(file, delimiter=';')
-    writer.writerow(['Model', 'Link', 'USD', 'UAH', 'Region'])
+  table = json2html.convert(json=items)
 
-    for item in items:
-      writer.writerow([
-        item['title'],
-        item['link'],
-        item['price usd'],
-        item['price uah'],
-        item['region'],
-      ])
+  with open(FILE, 'w', encoding='utf-8') as file:
+    file.write('%s\n' % table)
+    file.close()
+
+  print(f'File {FILE} has been created')
 
 def parse():
   html = get_html(URL)
   if html.status_code == 200:
     rows = []
-    pages_count = get_pages_count(html.text)
+    PAGES_COUNT = 2
 
-    for page in range(1, pages_count + 1):
-      print(f'Parsing page {page} from {pages_count}...')
+    for page in range(1, PAGES_COUNT + 1):
+      print(f'Parsing page {page} from {PAGES_COUNT}...')
 
       params = {
         'appid': 730,
         'page': page,
       }
       html = get_html(URL, params=params)
-      content = get_content(html.text)
-      rows.extend(content)
+      row = get_content(html.text)
+      rows.extend(row)
 
-    # save_file(rows, FILE)
     print(f'Got {len(rows)} data rows')
+    save_file(rows, FILE)
 
   else:
     print('Error:', html.status_code)
