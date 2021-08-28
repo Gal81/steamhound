64a7544b545044676437f4d3e233ac76cd64894a
Valhalleon <v.galuta@gmail.com>
2021-08-28 19:04:08 +0300

    Refactoring

--- csgoAPITest.py
+++ csgoAPITest.py
@@ -1,12 +1,16 @@
 import json
 import requests
 
-URL = 'https://api.csgofloat.com'
+HOST = 'https://api.csgofloat.com'
 HEADERS = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
   'Content-Type': 'application/json',
   'accept': '*/*'
 }
+
+LINK_HEAD = 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20'
+URL1 = LINK_HEAD + 'M3388383706163415278A19283545844D1030284652543335744'
+URL2 = LINK_HEAD + 'M3387257806257071617A23213490325D1030284652543335744'
 
 def print_response(data):
   if data.status_code == 200:
@@ -20,31 +24,31 @@ def print_response(data):
 def test_post():
   payload = {
     'links': [
-      { 'link': 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M3388383706163415278A19283545844D1030284652543335744' },
-      { 'link': 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M3387257806257071617A23213490325D1030284652543335744' }
+      { 'link': URL1 },
+      { 'link': URL2 }
     ]
   }
 
   response = requests.post(
-    URL + '/bulk',
+    HOST + '/bulk',
     headers=HEADERS,
-    data=payload,
+    json=payload,
   )
 
   print_response(response)
 
-test_post()
+# test_post()
 
 # TEST GET
 def test_get():
   response = requests.get(
-    URL,
+    HOST,
     headers=HEADERS,
     params={
-      'url': 'steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M3387257806257071617A23213490325D1030284652543335744'
+      'url': URL1
     }
   )
 
   print_response(response)
 
-# test_get()
+test_get()
